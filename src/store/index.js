0c68a986e80c1a7d540b2f85c0e8a8efd99d62a0
Natsuki <natsuki@somar.co.nz>
2021-06-19 22:51:35 +1200

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
+import state from './state';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+const store = new Vuex.Store({
+  actions,
+  getters,
+  mutations,
+  state
+});
+
+export default store;
